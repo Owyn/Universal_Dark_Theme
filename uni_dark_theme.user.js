b503e6690858432f74a9a282917534bf087f8125
Owyn <Owyn@users.noreply.github.com>
2025-02-24 16:28:00 +0300

    faster coloring @ page load
    - less flickering due to the website first being white then black (faster settings loading, placeholder dark color usage when it's still loading)
    - additional scrollbar coloring method added

--- uni_dark_theme.user.js
+++ uni_dark_theme.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		Universal Dark Theme Maker
 // @namespace	uni_dark_theme
-// @version		1.34
+// @version		1.4
 // @description	Simple Dark Theme style for any website which you can configure per-site
 // @downloadURL	https://github.com/Owyn/Universal_Dark_Theme/raw/master/uni_dark_theme.user.js
 // @updateURL	https://github.com/Owyn/Universal_Dark_Theme/raw/master/uni_dark_theme.user.js
@@ -10,8 +10,8 @@
 // @icon		https://images2.imgbox.com/b3/67/Aq5XazuW_o.png
 // @author		Owyn
 // @match		*://*/*
-// @grant		GM_getValue
-// @grant		GM_setValue
+// @grant		GM.getValue
+// @grant		GM.setValue
 // @grant		GM_addElement
 // @grant		GM_registerMenuCommand
 // @grant		unsafeWindow
@@ -26,11 +26,11 @@
 
 	var el;
 	var css;
-	var cfg_color;
-	var cfg_bgclr;
+	var cfg_color = "#c0c0c0";
+	var cfg_bgclr = "#2e2e2e";
 	var cfg_bgimg;
 	var cfg_bgtrans;
-	var cfg_visclr;
+	var cfg_visclr = "#a4a4a4";
 	var cfg_excl;
 	var cfg_css;
 	var cfg_js;
@@ -41,12 +41,13 @@
 		case "true": // back-compatibility
 		case "1":
 			cfg_active = true;
+			load_settings(); // start loading asap
 			break;
 		default:
 			cfg_active = false;
 			break;
 	}
-	function load_settings()
+	async function load_settings()
 	{
 		cfg_excl = localStorage.getItem('excl') || "";
 		cfg_css = localStorage.getItem('css') || "";
@@ -54,13 +55,28 @@
 		cfg_bgimg = (localStorage.getItem('bgimg') === '1');
 		cfg_bgtrans = (localStorage.getItem('bgtrans') === '1');
 		cfg_match_pseudo = (localStorage.getItem('match_pseudo') === '1');
-		if (typeof GM_getValue !== "undefined")
+		let done;
+		console.debug("UDT: GM settings started loading")
+		if (typeof GM !== "undefined" && typeof GM.getValue === "function")
 		{
-			cfg_color = GM_getValue("Color", "#c0c0c0");
-			cfg_bgclr = GM_getValue("bgColor", "#2e2e2e");
-			cfg_visclr = GM_getValue("visitedColor", "#a4a4a4");
+			done = Promise.all([
+				GM.getValue("Color", "#c0c0c0").then( function(result) { cfg_color = result; } , console.error),
+				GM.getValue("bgColor", "#2e2e2e").then( function(result) { cfg_bgclr = result; } , console.error),
+				GM.getValue("visitedColor", "#a4a4a4").then( function(result) { cfg_visclr = result; } , console.error)
+			]).then(
+				function() { console.debug("UDT: GM settings loaded"); updateHTMLColorVars(); },
+				function(error) { console.error("UDT: GM settings NOT loaded: " + error); }
+				);
 		}
+		return done
     }
+
+	function updateHTMLColorVars()
+	{
+		document.documentElement.style.setProperty('--cfg_color', cfg_color);
+		document.documentElement.style.setProperty('--cfg_bgclr', cfg_bgclr);
+		document.documentElement.style.setProperty('--cfg_visclr', cfg_visclr);
+	}
 
 	function activate(yes, prev_active)
 	{
@@ -76,7 +92,6 @@
 	}
 	function toggleDT()
 	{
-		load_settings();
 		cfg_active = !cfg_active;
 		activate(cfg_active, !cfg_active);
 		if(!cfg_active)
@@ -106,22 +121,24 @@
 		////////////// Main thing, the style!:
 		css = (cfg_excl !== "*" ?(cfg_bgtrans ?`
 		:root`+exc_txt+` {
-			color: `+cfg_color+` !important;
-			background`+bgimg_txt+`: `+cfg_bgclr+` !important;
-			border-color: `+cfg_color+` !important;
+			color: var(--cfg_color) !important;
+			background`+bgimg_txt+`: var(--cfg_bgclr) !important;
+			border-color: var(--cfg_color) !important;
+            scrollbar-color: var(--cfg_visclr) var(--cfg_bgclr) !important;
 			color-scheme: dark;
 		}
 		:root `:``)+`*`+exc_txt+match_pseudo+` {
-			color: `+(cfg_bgtrans ? `unset` : cfg_color)+` !important;
-			background`+bgimg_txt+`: `+(cfg_bgtrans ? `unset` : cfg_bgclr)+` !important;
-			border-color: `+cfg_color+` !important;
+			color: `+(cfg_bgtrans ? `unset` : `var(--cfg_color)`)+` !important;
+			background`+bgimg_txt+`: `+(cfg_bgtrans ? `unset` : `var(--cfg_bgclr)`)+` !important;
+			border-color: var(--cfg_color) !important;
+            scrollbar-color: var(--cfg_visclr) var(--cfg_bgclr) !important;
 			color-scheme: dark;
 		}
 		:visited`+exc_txt+`, a:hover`+exc_txt+` {
-			color: `+cfg_visclr+` !important;
+			color: var(--cfg_visclr) !important;
 		}
 		input:focus`+exc_txt+`,textarea:focus`+exc_txt+`,select:focus`+exc_txt+`{
-			outline: 1px solid `+cfg_visclr+` !important;
+			outline: 1px solid var(--cfg_visclr) !important;
 		}
 		`:"")+cfg_css;
 		//////////////
@@ -130,7 +147,6 @@
 	if(cfg_active)
 	{
 		console.debug("Adding dark style...");
-		load_settings();
 		make_css();
 		el = GM_addElement(document.documentElement, 'style', {textContent: css});
 		console.debug(unsafeWindow);
@@ -153,8 +169,8 @@
 
 	if (typeof GM_registerMenuCommand !== "undefined")
 	{
-		GM_registerMenuCommand("Dark Theme Configuration", ((typeof GM_addElement !== "undefined") ? cfg : tell_users_they_are_wrong), "D");
-		GM_registerMenuCommand("Toggle Dark Theme",  ((typeof GM_addElement !== "undefined") ? toggleDT : tell_users_they_are_wrong), "T");
+		GM_registerMenuCommand("Dark Theme Configuration", ((typeof GM_addElement === "function") ? () => load_settings().then(cfg, console.error) : tell_users_they_are_wrong), "D");
+		GM_registerMenuCommand("Toggle Dark Theme", ((typeof GM_addElement === "function") ? () => load_settings().then(toggleDT, console.error) : tell_users_they_are_wrong), "T");
 	}
 	else
 	{
@@ -172,17 +188,15 @@
 	var div;
 	function cfg()
 	{
-		function updateCfgDivColors()
-		{
-			div.style.setProperty('--cfg_color', cfg_color);
-			div.style.setProperty('--cfg_bgclr', cfg_bgclr);
-			div.style.setProperty('--cfg_visclr', cfg_visclr);
-		}
+		console.debug("UDT: config window open");
 		function saveCfg()
 		{
-			GM_setValue("Color", id("color").value);
-			GM_setValue("bgColor", id("bgclr").value);
-			GM_setValue("visitedColor", id("visitedColor").value);
+			if (typeof GM !== "undefined" && typeof GM.setValue === "function")
+			{
+				GM.setValue("Color", id("color").value);
+				GM.setValue("bgColor", id("bgclr").value);
+				GM.setValue("visitedColor", id("visitedColor").value);
+			}
 			localStorage.setItem('excl', id("excl").value);
 			localStorage.setItem('css', id("css").value);
 			localStorage.setItem('js', id("js").value);
@@ -207,7 +221,7 @@
 			activate(id("active").checked, cfg_active );
 			cfg_active = id("active").checked;
 			//
-			updateCfgDivColors();
+			updateHTMLColorVars();
 			// clean up
 			if(!id("active").checked) { localStorage.removeItem('active'); }
 			if(!id("match_pseudo").checked) { localStorage.removeItem('match_pseudo'); }
@@ -222,13 +236,12 @@
 			if(!div.isConnected) document.body.appendChild(div); // it was there but got closed
 			return; // div already built
 		}
-		load_settings();
 		div = document.createElement("div");
 		div.style = "all: initial !important; margin: auto !important; width: fit-content !important; height: fit-content !important; border: 1px solid var(--cfg_color) !important; background: var(--cfg_bgclr) !important; position: fixed !important; inset: 0 !important; z-index: 2147483647 !important; line-height: 1 !important;"; // all:initial to disable CSS inheritance
 		div.attachShadow({mode: 'open'}); // outer CSS selectors won't affect it now (but we will still inherit styles) // we could use an iframe but extensions might block it? <iframe src='data:text/html,<strong>Hello World!</strong>'></iframe>
 		const id = div.shadowRoot.getElementById.bind(div.shadowRoot);
 		let shadowStyle = `* {
-			color-scheme: dark; 
+			color-scheme: dark;
 			color: var(--cfg_color);
 			background: var(--cfg_bgclr);
 			border-color: var(--cfg_color);
@@ -241,7 +254,7 @@
 		input[type='checkbox'] { padding: 0px; }
 		div { margin: auto; display: table; }
 		div > input { margin:2px; width: 6em; }
-		textarea { margin: 0px; min-width: 400px; height: 50px; resize:both; }`;
+		textarea { margin: 0px; min-width: 400px; min-height: 1em; height: 50px; resize:both; }`;
 		div.shadowRoot.innerHTML = `<b><br><center>Configuration</center></b>
 		<div>
 		<br><input id='color' type='text'> Text color (empty = site default)
@@ -258,7 +271,6 @@
 		<br><input id='cfg_save' type='button' value='Save config'> <input id='cfg_close' type='button' value='Close'></center>`;
 		document.body.appendChild(div);
 		GM_addElement(div.shadowRoot, 'style', {textContent: shadowStyle}); // damn CSP gets us even in our own shadow...
-		updateCfgDivColors();
 		id("color").value = cfg_color;
 		id("bgclr").value = cfg_bgclr;
 		id("bgimg").checked = cfg_bgimg;
